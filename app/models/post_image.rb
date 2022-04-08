2ff7c12e069267658dc2542a727c1bf09628d6e4
ChihiroYoshioka <yohokaaaa@gmail.com>
2022-04-08 07:09:44 +0000

    [Finish]4-8

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,3 +1,3 @@
 class PostImage < ApplicationRecord
-  has_one_attached:image
+  has_one_attached :image
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_08_010826) do
+ActiveRecord::Schema.define(version: 2022_04_08_070052) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -41,6 +41,9 @@ ActiveRecord::Schema.define(version: 2022_04_08_010826) do
   end
 
   create_table "post_images", force: :cascade do |t|
+    t.string "shop_name"
+    t.text "caption"
+    t.integer "user_"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
